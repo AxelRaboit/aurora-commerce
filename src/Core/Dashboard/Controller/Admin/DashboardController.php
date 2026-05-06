bd1e0d69dc4cf07941e1c64e1212592d800b581c
Axel Raboit <axel.raboit@gmail.com>
2026-05-06 02:40:47 +0200

    fix(dashboard): open dashboard to all ROLE_USER, impersonated users can land there

--- src/Core/Dashboard/Controller/Admin/DashboardController.php
+++ src/Core/Dashboard/Controller/Admin/DashboardController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/backend', name: 'backend_')]
-#[IsGranted('core.dashboard.view')]
+#[IsGranted('ROLE_USER')]
 class DashboardController extends AbstractController
 {
     public function __construct(private readonly DashboardViewBuilder $viewBuilder) {}
