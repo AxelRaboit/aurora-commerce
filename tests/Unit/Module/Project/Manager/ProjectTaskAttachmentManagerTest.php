8b6ad84d60ee759c992e8b9a6bee5c68c7fd4fbd
Axel Raboit <axel.raboit@gmail.com>
2026-05-15 18:09:49 +0200

    test: add unit tests for ProjectSavedViewManager, ProjectLabelManager, ProjectTaskAttachmentManager, ProjectTaskTimeEntryManager

---
+++ tests/Unit/Module/Project/Manager/ProjectTaskAttachmentManagerTest.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aurora\Tests\Unit\Module\Project\Manager;
+
+use Aurora\Core\Audit\Service\AuditLogger;
+use Aurora\Core\Media\Entity\MediaInterface;
+use Aurora\Core\Media\Repository\MediaRepository;
+use Aurora\Module\Project\Entity\Project;
+use Aurora\Module\Project\Entity\ProjectTask;
+use Aurora\Module\Project\Manager\ProjectTaskAttachmentManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+final class ProjectTaskAttachmentManagerTest extends TestCase
+{
+    private function makeMedia(int $id): MediaInterface
+    {
+        $media = $this->createStub(MediaInterface::class);
+        $media->method('getId')->willReturn($id);
+
+        return $media;
+    }
+
+    public function testAttachReturnsZeroForEmptyArray(): void
+    {
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects(self::never())->method('flush');
+
+        $repo = $this->createStub(MediaRepository::class);
+        $audit = $this->createMock(AuditLogger::class);
+        $audit->expects(self::never())->method('log');
+
+        $manager = new ProjectTaskAttachmentManager($em, $repo, $audit);
+        $task = (new ProjectTask())->setProject(new Project());
+
+        self::assertSame(0, $manager->attach($task, []));
+    }
+
+    public function testAttachAddsNewMedia(): void
+    {
+        $media1 = $this->makeMedia(1);
+        $media2 = $this->makeMedia(2);
+
+        $repo = $this->createStub(MediaRepository::class);
+        $repo->method('findBy')->willReturn([$media1, $media2]);
+
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects(self::once())->method('flush');
+
+        $audit = $this->createMock(AuditLogger::class);
+        $audit->expects(self::once())
+            ->method('log')
+            ->with('project', 'task.attachment.added', 'ProjectTask', self::anything(), self::anything());
+
+        $manager = new ProjectTaskAttachmentManager($em, $repo, $audit);
+        $task = (new ProjectTask())->setProject(new Project());
+
+        $added = $manager->attach($task, [1, 2]);
+
+        self::assertSame(2, $added);
+        self::assertCount(2, $task->getAttachments());
+    }
+
+    public function testAttachSkipsExistingMedia(): void
+    {
+        $existing = $this->makeMedia(1);
+        $new = $this->makeMedia(2);
+
+        $repo = $this->createStub(MediaRepository::class);
+        $repo->method('findBy')->willReturn([$existing, $new]);
+
+        $em = $this->createMock(EntityManagerInterface::class);
+        $audit = $this->createMock(AuditLogger::class);
+
+        $task = (new ProjectTask())->setProject(new Project());
+        $task->addAttachment($existing);
+
+        $manager = new ProjectTaskAttachmentManager($em, $repo, $audit);
+        $added = $manager->attach($task, [1, 2]);
+
+        self::assertSame(1, $added, 'only new media counted');
+    }
+
+    public function testDetachDoesNothingForUnknownMedia(): void
+    {
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects(self::never())->method('flush');
+
+        $audit = $this->createMock(AuditLogger::class);
+        $audit->expects(self::never())->method('log');
+
+        $task = (new ProjectTask())->setProject(new Project());
+
+        $manager = new ProjectTaskAttachmentManager($em, $this->createStub(MediaRepository::class), $audit);
+        $manager->detach($task, $this->makeMedia(1));
+    }
+
+    public function testDetachRemovesMediaAndAudits(): void
+    {
+        $media = $this->makeMedia(1);
+
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects(self::once())->method('flush');
+
+        $audit = $this->createMock(AuditLogger::class);
+        $audit->expects(self::once())
+            ->method('log')
+            ->with('project', 'task.attachment.removed', 'ProjectTask', self::anything(), self::anything());
+
+        $task = (new ProjectTask())->setProject(new Project());
+        $task->addAttachment($media);
+
+        $manager = new ProjectTaskAttachmentManager($em, $this->createStub(MediaRepository::class), $audit);
+        $manager->detach($task, $media);
+
+        self::assertCount(0, $task->getAttachments());
+    }
+}
