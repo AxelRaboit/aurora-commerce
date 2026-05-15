9ce2195bf77d4ca42ed4db55eb21fd02e8eeaee4
Axel Raboit <axel.raboit@gmail.com>
2026-05-15 17:27:46 +0200

    test: add unit tests for Tiers, Notification, InvoiceLine entities

---
+++ tests/Unit/Entity/NotificationTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aurora\Tests\Unit\Entity;
+
+use Aurora\Core\Notification\Entity\Notification;
+use Aurora\Core\User\Entity\User;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+final class NotificationTest extends TestCase
+{
+    public function testIdIsNullByDefault(): void
+    {
+        self::assertNull((new Notification())->getId());
+    }
+
+    public function testDefaultValues(): void
+    {
+        $notification = new Notification();
+
+        self::assertNull($notification->getBody());
+        self::assertNull($notification->getUrl());
+        self::assertNull($notification->getData());
+        self::assertNull($notification->getReadAt());
+    }
+
+    public function testRecipientGetterAndSetter(): void
+    {
+        $user = new User();
+        $notification = (new Notification())->setRecipient($user);
+
+        self::assertSame($user, $notification->getRecipient());
+    }
+
+    public function testTypeAndTitleGettersAndSetters(): void
+    {
+        $notification = (new Notification())->setType('comment.new')->setTitle('New comment');
+
+        self::assertSame('comment.new', $notification->getType());
+        self::assertSame('New comment', $notification->getTitle());
+    }
+
+    public function testBodyAndUrlGettersAndSetters(): void
+    {
+        $notification = (new Notification())
+            ->setBody('Someone commented on your post')
+            ->setUrl('/backend/posts/42');
+
+        self::assertSame('Someone commented on your post', $notification->getBody());
+        self::assertSame('/backend/posts/42', $notification->getUrl());
+    }
+
+    public function testDataGetterAndSetter(): void
+    {
+        $data = ['postId' => 42, 'authorId' => 7];
+        $notification = (new Notification())->setData($data);
+
+        self::assertSame($data, $notification->getData());
+
+        $notification->setData(null);
+        self::assertNull($notification->getData());
+    }
+
+    public function testMarkAsReadSetsTimestamp(): void
+    {
+        $notification = new Notification();
+
+        $before = new DateTimeImmutable();
+        $notification->markAsRead();
+        $after = new DateTimeImmutable();
+
+        self::assertNotNull($notification->getReadAt());
+        self::assertGreaterThanOrEqual($before->getTimestamp(), $notification->getReadAt()->getTimestamp());
+        self::assertLessThanOrEqual($after->getTimestamp(), $notification->getReadAt()->getTimestamp());
+    }
+}
