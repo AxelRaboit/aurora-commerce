9e78f1b942d0821c5386f6a5990660a71c844bc3
Axel Raboit <axel.raboit@gmail.com>
2026-05-15 18:15:19 +0200

    test: add unit tests for MountPointTestResult, ServiceInput, ServiceInputFactory, ProjectLabel/Column/Sprint/TaskComment/TaskTimeEntry inputs, AgencyInput

---
+++ tests/Unit/Core/Service/Dto/ServiceInputFactoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aurora\Tests\Unit\Core\Service\Dto;
+
+use Aurora\Core\Service\Dto\ServiceInputFactory;
+use PHPUnit\Framework\TestCase;
+
+final class ServiceInputFactoryTest extends TestCase
+{
+    public function testFromArrayTrimsName(): void
+    {
+        $input = (new ServiceInputFactory())->fromArray(['name' => '  Hello  ']);
+
+        self::assertSame('Hello', $input->getName());
+    }
+
+    public function testFromArrayWithMissingNameReturnsEmpty(): void
+    {
+        $input = (new ServiceInputFactory())->fromArray([]);
+
+        self::assertSame('', $input->getName());
+    }
+}
