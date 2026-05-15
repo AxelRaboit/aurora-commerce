d435dc2af06009b84033e0e54a86542861b51374
Axel Raboit <axel.raboit@gmail.com>
2026-05-15 18:27:42 +0200

    test: add unit tests for CreateUserInput, UserInviteInput, MoodInput, UserSetPasswordInput, VaultFolderInput

---
+++ tests/Unit/Core/User/Dto/MoodInputTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aurora\Tests\Unit\Core\User\Dto;
+
+use Aurora\Core\User\Dto\MoodInput;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+final class MoodInputTest extends TestCase
+{
+    public function testFromRequestParsesMoodMessage(): void
+    {
+        $request = Request::create('/', 'POST', [], [], [], [], json_encode(['moodMessage' => '  Hello world  ']));
+
+        $input = MoodInput::fromRequest($request);
+
+        self::assertSame('Hello world', $input->moodMessage);
+    }
+
+    public function testFromRequestWithEmptyContent(): void
+    {
+        $request = Request::create('/', 'POST');
+
+        $input = MoodInput::fromRequest($request);
+
+        self::assertNull($input->moodMessage);
+    }
+
+    public function testFromRequestWithMissingField(): void
+    {
+        $request = Request::create('/', 'POST', [], [], [], [], json_encode([]));
+
+        $input = MoodInput::fromRequest($request);
+
+        self::assertNull($input->moodMessage);
+    }
+
+    public function testConstructorDefaults(): void
+    {
+        self::assertNull((new MoodInput())->moodMessage);
+    }
+}
